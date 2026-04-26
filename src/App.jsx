dab75df561688530885d4f69c5a5eacc7b454839
farooque1 <sfarooque65@gmail.com>
2026-04-26 18:21:41 +0530

    url added

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,28 @@
-import React, { useState, useEffect } from 'react';
-import io from 'socket.io-client';
-import axios from 'axios';
-import ChatBox from './components/ChatBox';
-import MessageInput from './components/MessageInput';
-import './index.css';
+import React, { useState, useEffect } from "react";
+import io from "socket.io-client";
+import axios from "axios";
+import ChatBox from "./components/ChatBox";
+import MessageInput from "./components/MessageInput";
+import "./index.css";
 
-const socket = io('http://localhost:5000');
+const socket = io("https://chat-app-backend-nhmx.onrender.com/");
 
 function App() {
   const [messages, setMessages] = useState([]);
 
   useEffect(() => {
     // Fetch initial messages from API
-    axios.get('http://localhost:5000/api/messages')
-      .then(res => setMessages(res.data))
-      .catch(err => console.error("Failed fetching messages", err));
+    axios
+      .get("https://chat-app-backend-nhmx.onrender.com//api/messages")
+      .then((res) => setMessages(res.data))
+      .catch((err) => console.error("Failed fetching messages", err));
 
     // Listen for new messages
-    socket.on('receiveMessage', (message) => {
-      setMessages(prev => [...prev, message]);
+    socket.on("receiveMessage", (message) => {
+      setMessages((prev) => [...prev, message]);
     });
 
-    return () => socket.off('receiveMessage');
+    return () => socket.off("receiveMessage");
   }, []);
 
   const handleSendMessage = async (text, file) => {
@@ -29,9 +30,12 @@ function App() {
 
     if (file) {
       const formData = new FormData();
-      formData.append('file', file);
+      formData.append("file", file);
       try {
-        const res = await axios.post('http://localhost:5000/api/upload', formData);
+        const res = await axios.post(
+          "https://chat-app-backend-nhmx.onrender.com//api/upload",
+          formData,
+        );
         fileUrl = res.data.fileUrl;
       } catch (err) {
         console.error("Upload failed", err);
@@ -41,15 +45,13 @@ function App() {
     }
 
     if (text || fileUrl) {
-      socket.emit('sendMessage', { message: text, fileUrl });
+      socket.emit("sendMessage", { message: text, fileUrl });
     }
   };
 
   return (
     <div className="app-container">
-      <div className="header">
-        Modern Chat
-      </div>
+      <div className="header">Modern Chat</div>
       <ChatBox messages={messages} />
       <MessageInput onSendMessage={handleSendMessage} />
     </div>
